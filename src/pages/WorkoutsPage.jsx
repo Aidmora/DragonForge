705a0ed0175255facef43d6b761b6b560e90c03c
Ariel David Mora Jarrín <arielabc389@gmail.com>
2025-05-17 19:04:16 -0500

    feat(información): aumento de fila para informacion - bootstrap

--- src/pages/WorkoutsPage.jsx
+++ src/pages/WorkoutsPage.jsx
@@ -1,33 +1,37 @@
-import React from 'react';
-import MenuBar from '../components/MenuBar';
-import Carousel from '../components/Carousel';
-import './Workouts.css';
+import React from "react";
+import MenuBar from "../components/MenuBar";
+import Carousel from "../components/Carousel";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import "./Workouts.css";
 
 export default function WorkoutsPage() {
   return (
     <div className="page-container">
       <MenuBar />
       <main className="site-main">
-        <div className="content-wrapper">
-          <div className="row">
-            <div className="col-md-12 text-bg-primary">
-            <Carousel />
-          </div>
-          </div>
-          <div className="row mb-4">
-            <div className="col-md-6 text-bg-primary">
+        <Carousel />
+        <div className="container content-wrapper">
+          <div className="row com">
+            <div className="col-6">
+              <p>Interesado en entrar a DragonForge?</p>
             </div>
-            <div className="col-md-6 text-bg-primary">
+            <div className="col-6">
+              <button class="botonInfo">ver más</button>
             </div>
           </div>
-          <div className="col-md-12 text-bg-primary">
+          <div className="row comD">
+            <div className="col-md-6 colm1">
+              <h1>Columna 1</h1>
+            </div>
+            <div className="col-md-6 colm2">
+              <h1>Columna 2</h1>
+            </div>
           </div>
         </div>
       </main>
       <footer className="site-footer">
-        <div className="content-wrapper">
-          © 2025 Dragon Forge
-        </div>
+        <div className="content-wrapper">© 2025 Dragon Forge</div>
       </footer>
     </div>
   );
